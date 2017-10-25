e70fb0238a7c5ade621562f3d0dd4f7e59db4f0e
renanbronchart <renan.bronchart@gmail.com>
2017-10-25 23:52:58 +0200

    [Fix:Circle] Add circle with hover

--- exemple_0/bundle.js
+++ exemple_0/bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function () {
-  const tab = [30, 40, 50, 60]
+  const tab = [30, 40, 20, 70, 10, 5, 30, 60]
 
   const body = d3.select('body')
   let paras = body.selectAll('p')
@@ -8,8 +8,36 @@
                   .enter()
                   .append('p')
                   .text((d, i) => {
-                    return `Le texte ${i} contient la valeur ${d}`
+                    return `Le rond n°${i} a pour rayon ${d}px`
                   })
+  let svg = body.append('svg')
+                .attr({
+                  'width': '800px',
+                  'height': '800px',
+                  'border': '1px solid black'
+                })
+  let circles = svg.selectAll('circle')
+                    .data(tab)
+                    .enter()
+                    .append('circle')
+                    .attr({
+                      'fill': 'black',
+                    })
+                    .attr('cx', (d, i) => {
+                      return 30+d+i*70
+                    })
+                    .attr('cy', '200')
+                    .attr('r', (d, i) => {
+                      return `${d/3}px`
+                    })
+                    .on('mouseover', function () {
+                      d3.select(this).attr('fill', 'red')
+                    })
+                    .on('mouseout', function () {
+                      d3.select(this).attr('fill', 'black')
+                    })
+
+
   paras
     .on('mouseover', function () {
       d3.select(this).style('color', 'red')
--- exemple_0/script15.js
+++ exemple_0/script15.js
@@ -1,5 +1,5 @@
 (function () {
-  const tab = [30, 40, 50, 60]
+  const tab = [30, 40, 20, 70, 10, 5, 30, 60]
 
   const body = d3.select('body')
   let paras = body.selectAll('p')
@@ -7,8 +7,36 @@
                   .enter()
                   .append('p')
                   .text((d, i) => {
-                    return `Le texte ${i} contient la valeur ${d}`
+                    return `Le rond n°${i} a pour rayon ${d}px`
                   })
+  let svg = body.append('svg')
+                .attr({
+                  'width': '800px',
+                  'height': '800px',
+                  'border': '1px solid black'
+                })
+  let circles = svg.selectAll('circle')
+                    .data(tab)
+                    .enter()
+                    .append('circle')
+                    .attr({
+                      'fill': 'black',
+                    })
+                    .attr('cx', (d, i) => {
+                      return 30+d+i*70
+                    })
+                    .attr('cy', '200')
+                    .attr('r', (d, i) => {
+                      return `${d/3}px`
+                    })
+                    .on('mouseover', function () {
+                      d3.select(this).attr('fill', 'red')
+                    })
+                    .on('mouseout', function () {
+                      d3.select(this).attr('fill', 'black')
+                    })
+
+
   paras
     .on('mouseover', function () {
       d3.select(this).style('color', 'red')
