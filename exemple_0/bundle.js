be50ff47e67291b65b2199369196e729f40a972e
renanbronchart <renan.bronchart@gmail.com>
2017-10-26 21:25:15 +0200

    [Feat:Global] Change to v4 of d3.js and import multiple attributes

--- exemple_0/bundle.js
+++ exemple_0/bundle.js
@@ -3,16 +3,16 @@
   const body = d3.select('body')
   const svg = body.append('svg')
 
-  svg.attr({
-    'width': '600px',
-    'height': '600px'
+  svg.attrs({
+    width: 600,
+    height: 600
   })
 
-  const path = d3.geo.path()
-  const projection = d3.geo.conicConformal()
-                            .center([2.454071, 46.279229])
-                            .scale(3000)
-                            .translate([300, 300])
+  const path = d3.geoPath()
+  const projection = d3.geoConicConformal()
+                        .center([2.454071, 46.279229])
+                        .scale(3000)
+                        .translate([300, 300])
 
   path.projection(projection)
 
